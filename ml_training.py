31a385f356db4a2fac532557d098b913c7433fcc
mikechen092 <mchen092@gmail.com>
2018-07-13 14:42:04 -0500

    updating ml_training to include x,y location of pixel

--- ml_training.py
+++ ml_training.py
@@ -6,26 +6,26 @@ import numpy as np
 import glob, os
 import sys
 import re
-import cPickle as pickle
+import pickle as pickle
 
 def make_input(data):
-    assert data.shape[1] == 3
+    assert data.shape[1] == 5
     # make data into 0's/1's
     data = data.astype(np.float32) / 255.0
 
-    # columns 0 1 2 0*0 0*1 0*2 1*1 1*2 2*2
+    # columns 0 1 2 3 4 0*0 0*1 0*2 0*3 0*4 1*1 1*2 1*3 1*4 2*2 2*3 2*4 3*3 3*4 4*4
     d0 = data[:,0].reshape(-1,1)
     d1 = data[:,1].reshape(-1,1)
     d2 = data[:,2].reshape(-1,1)
 
+    d3 = data[:,3].reshape(-1,1)
+    d4 = data[:,4].reshape(-1,1)
+
     #return data
-    result = np.hstack((data, d0*d0, d0*d1, d0*d2, d1*d1, d1*d2, d2*d2))
+    result = np.hstack((data, d0*d0, d0*d1, d0*d2, d0*d3, d0*d4, d1*d1, d1*d2, d1*d3, d1*d4, d2*d2, d2*d3, d2*d4, d3*d3, d3*d4, d4*d4))
 
-    print 'data shape is: ', data.shape
-    print 'result shape is: ', result.shape
-
-    print data[:5]
-    print result[:5]
+    print('data shape is: ', data.shape)
+    print('result shape is: ', result.shape)
 
     return result
 
@@ -44,22 +44,31 @@ for filename in glob.glob('ml_data/target/*.[jJ][pP][gG]'):
 
     matches = glob.glob('ml_data/test/' + prefix + '*.[jJ][pP][gG]')
 
+    # labels are reading in target -> what the pixel is classified as
     labels = (cv2.imread(filename, cv2.IMREAD_GRAYSCALE) > 127).astype(np.uint8)
+
+    # matches reading in test -> actual rgb values of image
     rgb = cv2.imread(matches[0])
+
 
     # appending images to a larger numpy data structrure
     nrows = len(labels)
     assert len(rgb) == nrows
 
-    labels = labels[nrows/2:]
-    rgb = rgb[nrows/2:]
+    # add additional classification using the location of the pixel
+    rgb_location = np.zeros((len(rgb),len(rgb[0]),5))
+    for i in range(len(rgb_location)):
+        for j in range(len(rgb_location[i])):
+            rgb_location[i][j][3] = i
+            rgb_location[i][j][4] = j
+            rgb_location[i][j][:3] = rgb[i][j]
 
-    # tall nx3 arrays
-    rgb = rgb.reshape(-1, 3)
+    rgb = rgb_location.reshape(-1,5)
 
     # wide flat array of length n
     labels = labels.flatten()
 
+    # aggregate all the information
     all_train_data.append(rgb)
     all_train_labels.append(labels)
 
@@ -67,11 +76,13 @@ for filename in glob.glob('ml_data/target/*.[jJ][pP][gG]'):
 all_train_data = np.vstack(tuple(all_train_data))
 all_train_labels = np.hstack(tuple(all_train_labels))
 
-print all_train_data.shape
-print all_train_labels.shape
+print(all_train_data.shape)
+print(all_train_labels.shape)
 
-train_samples = 100000
+train_samples = 500000
 
+# if the number of defined samples is less than the number available
+# randomly choose the sample
 if len(all_train_labels) > train_samples:
 
     index = np.arange(len(all_train_labels))
@@ -81,11 +92,9 @@ if len(all_train_labels) > train_samples:
     all_train_data = all_train_data[index]
     all_train_labels = all_train_labels[index]
 
-print all_train_data.shape
-print all_train_labels.shape
+print(all_train_data.shape)
+print(all_train_labels.shape)
 
-#poly_svm = svm.SVC(kernel='poly', degree=2, C=1.0)
-#poly_svm = svm.SVC(kernel='rbf')
 poly_svm = svm.LinearSVC()
 
 print("TRAINING ON MANY IMAGES")
@@ -96,7 +105,13 @@ poly_svm.fit(make_input(all_train_data),
 print('HERE ARE RESULTS:')
 for filename in glob.glob('ml_data/test_set/*.[jJ][pP][gG]'):
     test_rgb = cv2.imread(filename)
-    result = poly_svm.predict(make_input(test_rgb.reshape(-1, 3)))
+    test_rgb_location = np.zeros((len(test_rgb),len(test_rgb[0]),5))
+    for i in range(len(test_rgb_location)):
+        for j in range(len(test_rgb_location[i])):
+            test_rgb_location[i][j][3] = i
+            test_rgb_location[i][j][4] = j
+            test_rgb_location[i][j][:3] = test_rgb[i][j]
+    result = poly_svm.predict(make_input(test_rgb_location.reshape(-1, 5)))
 
     img = result.reshape(test_rgb.shape[:2]) * 255
 
@@ -108,9 +123,15 @@ for filename in glob.glob('ml_data/test_set/*.[jJ][pP][gG]'):
 
 for filename in glob.glob('ml_data/test/*.[jJ][pP][gG]'):
     test_rgb = cv2.imread(filename)
-    result = poly_svm.predict(make_input(test_rgb.reshape(-1, 3)))
+    test_rgb_location = np.zeros((len(test_rgb),len(test_rgb[0]),5))
+    for i in range(len(test_rgb_location)):
+        for j in range(len(test_rgb_location[i])):
+            test_rgb_location[i][j][3] = i
+            test_rgb_location[i][j][4] = j
+            test_rgb_location[i][j][:3] = test_rgb[i][j]
+    result = poly_svm.predict(make_input(test_rgb_location.reshape(-1, 5)))
 
-    img = result.reshape(test_rgb.shape[:2]) * 255
+    img = result.reshape(test_rgb_location.shape[:2]) * 255
 
     img_color = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
     # img_color[:len(img_color)/4] = 0
